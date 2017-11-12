23ef5134ae40da9bee648ba0c4d70ac292b4f799
Marcel Cutts <marcel.cutts@gmail.com>
2017-11-12 03:20:22 +0000

    Add bare content and container

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,51 +5,25 @@ import Helmet from 'react-helmet'
 
 import './index.css'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
-
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Marcel Cutts"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Marcel Cutts\'s personal site' },
+        { name: 'keywords', content: 'Personal site, work, history, programming, engineering' },
       ]}
     />
-    <Header />
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
       }}
     >
       {children()}
