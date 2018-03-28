30d156a1ca81bea8a3b0b368d2eab5116371cbfe
Marcel Cutts <marcel.cutts@gmail.com>
2018-03-28 12:38:01 -0400

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,22 @@ module.exports = {
   siteMetadata: {
     title: `Marcel Cutts`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [`gatsby-plugin-react-helmet`, {
+    resolve: `gatsby-plugin-favicon`,
+    options: {
+      logo: "./src/favicon.png",
+      injectHTML: true,
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    }
+  }],
 }
