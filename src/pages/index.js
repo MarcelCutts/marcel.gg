3b2ac302ffdb33d4898b6dda339a07593af7a759
Marcel Cutts <marcel.cutts@gmail.com>
2018-03-28 12:37:46 -0400

    Add basic links for desktop

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,23 @@ import Link from 'gatsby-link'
 
 import './index.css'
 
+
+const buttonContent = [
+  { text: "Twitter", url: "https://twitter.com/marcelcutts"},
+  { text: "Talks", url: "https://speakerdex.co/marcel"},
+  { text: "Medium", url: "https://medium.com/@marcel.cutts/latest"},
+  { text: "Email", url: "mailto:marcel@asgard.tech"} 
+];
+
+const ExternalLink = ({content}) => <a href={content.url} className="externalLink"> {content.text} </a>;
+
 const IndexPage = () => (
   <div className="index">
     <h1>Marcel Cutts</h1>
     <hr />
     <p>Technologist with a relentless vendetta against mediocrity.</p>
+    <hr />
+    <div className="externalLinkContainer"> {buttonContent.map(c => <ExternalLink content={c} key={c.text} />)} </div>
   </div>
 )
 
